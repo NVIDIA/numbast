242ef8d50fae99c083ea7e4272395f1400c5081d
Michael Yh Wang <wangm@nvidia.com>
2024-04-10 01:18:31 +0800

    use yq to update cuda version in envrionment.yaml

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -7,12 +7,12 @@ set -euo pipefail
 rapids-logger "Create checks conda environment"
 . /opt/conda/etc/profile.d/conda.sh
 
-rapids-dependency-file-generator \
-  --output conda \
-  --file_key checks \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
+# Update CUDAToolkit version according to CI environment.
+# TODO: we should use some type of yaml template engine to simplify this.
+yq -i -y '.dependencies |= map(if startswith("cuda-version") then "cuda-version=${RAPIDS_CUDA_VERSION%.*}" else . end)' conda/environment.yaml
+yq -i -y '.dependencies |= map(if startswith("cuda-python") then "cuda-python=${RAPIDS_CUDA_VERSION%.*}" else . end)' conda/environment.yaml
 
-rapids-mamba-retry env create --yes -f env.yaml -n checks
+rapids-mamba-retry env create --yes -f conda/environment.yaml -n checks
 conda activate checks
 
 # Run pre-commit checks
