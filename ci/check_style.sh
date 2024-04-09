fed420c86034a15632e43e4af43eaadada75e279
Michael Yh Wang <wangm@nvidia.com>
2024-04-10 01:42:56 +0800

    use yq 4.x expression syntax

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -11,8 +11,8 @@ yq --version
 
 # Update CUDAToolkit version according to CI environment.
 # TODO: we should use some type of yaml template engine to simplify this.
-yq -i -y '.dependencies |= map(if startswith("cuda-version") then "cuda-version=${RAPIDS_CUDA_VERSION%.*}" else . end)' conda/environment.yaml
-yq -i -y '.dependencies |= map(if startswith("cuda-python") then "cuda-python=${RAPIDS_CUDA_VERSION%.*}" else . end)' conda/environment.yaml
+yq e '(.dependencies[] | select(. == "cuda-version*")) |= "cuda-version=${RAPIDS_CUDA_VERSION%.*}"' conda/environment.yaml -i
+yq e '(.dependencies[] | select(. == "cuda-python*")) |= "cuda-python=${RAPIDS_CUDA_VERSION%.*}"' conda/environment.yaml -i
 
 rapids-mamba-retry env create --yes -f conda/environment.yaml -n checks
 conda activate checks
