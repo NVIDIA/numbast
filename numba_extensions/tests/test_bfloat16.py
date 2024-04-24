6c6a2c58242a80a6fc7ef9888358d33ff161fa03
Benjamin Zaitlen <quasiben@gmail.com>
2024-04-24 01:37:14 +0000

    expose numba type (nb_type) and add PyTorch Test

--- numba_extensions/tests/test_bfloat16.py
+++ numba_extensions/tests/test_bfloat16.py
@@ -37,6 +37,12 @@ import numpy as np
 from numba import int16, int32, int64, uint16, uint32, uint64, float32, float64
 from numba.types import float16
 
+from ml_dtypes import bfloat16
+from numba.np import numpy_support
+from bf16.bf16_bindings import __nv_bfloat16
+# what is the constructor vs what is the numba type ?
+numpy_support.FROM_DTYPE[np.dtype('bfloat16')] = nv_bfloat16.nb_type
+
 
 def test_ctor():
     @cuda.jit(link=get_shims())
@@ -219,3 +225,59 @@ def test_math_func():
     np.testing.assert_allclose(
         a[12:], [np.exp(x), np.exp2(x), np.power(10, x)], atol=1e2
     )
+
+
+
+# implement proxy object for bf16
+# proxy should implement CAI which numba will consume directly
+# .__cuda_array_interface__
+
+import torch
+class ProxyTorch(torch.Tensor):
+    def __init__(self, tensor):
+        self._tensor = tensor
+
+    def __getattr__(self, attr):
+        if attr is "__cuda_array_interface__":
+            return self.__cuda_array_interface__
+
+        return super(ProxyTorch, self).__getattr__(attr)
+
+    @property
+    def __cuda_array_interface__(self):
+        typestr = 'bfloat16'
+
+        if self._tensor.is_contiguous():
+            # __cuda_array_interface__ v2 requires the strides to be omitted
+            # (either not set or set to None) for C-contiguous arrays.
+            strides = None
+        else:
+            strides = tuple(s * itemsize for s in self._tensor.stride())
+        shape = tuple(self.shape)
+        data_ptr = self._tensor.data_ptr() if self._tensor.numel() > 0 else 0
+        data = (data_ptr, False)  # read-only is false
+        return dict(typestr=typestr, shape=shape, strides=strides, data=data,
+                version=2)
+
+def test_torchbf16():
+    torch = pytest.importorskip("torch")
+
+    @cuda.jit(link=get_shims())
+    def torch_add(a, b, out):
+        i, j = cuda.grid(2)
+        if i < out.shape[0] and j < out.shape[1]:
+            out[i, j] = a[i, j] + b[i, j]
+
+    a = torch.ones([2,2], device=torch.device('cuda:0'), dtype=torch.bfloat16)
+    b = torch.ones([2,2], device=torch.device('cuda:0'), dtype=torch.bfloat16)
+    aa = ProxyTorch(a)
+    bb = ProxyTorch(b)
+    twos = aa + bb
+
+    out = torch.zeros([2,2], device=torch.device('cuda:0'), dtype=torch.bfloat16)
+    out = ProxyTorch(out)
+
+    torch_add[16, 16](aa, bb, out)
+    print(out)
+    print("wait...")
+
