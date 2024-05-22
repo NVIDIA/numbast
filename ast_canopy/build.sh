847d806b8650c88494153cd2c3785848a561c138
Michael Yh Wang <wangm@nvidia.com>
2024-05-22 14:29:29 +0800

    remove build.sh

--- ast_canopy/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-python -m pip install ast_canopy/ -vv && \
-    mv $SP_DIR/libastcanopy.so $PREFIX/lib
