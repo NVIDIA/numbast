0a2822bd8e9abb64494eb24c3835076ee03e0731
Michael Yh Wang <wangm@nvidia.com>
2024-05-22 10:39:44 +0800

    use local python bin

--- ast_canopy/build.sh
+++ ast_canopy/build.sh
@@ -2,5 +2,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-$PYTHON -m pip install ast_canopy/ -vv && \
+python -m pip install ast_canopy/ -vv && \
     mv $SP_DIR/libastcanopy.so $PREFIX/lib
