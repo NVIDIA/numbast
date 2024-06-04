8ae75fd9237a441e280529588d4f736d052e615b
Michael Yh Wang <wangm@nvidia.com>
2024-06-04 23:46:19 +0800

    [skipci] add back skip-existing

--- ci/upload_conda.sh
+++ ci/upload_conda.sh
@@ -8,10 +8,9 @@ CONDA_TOKEN="is-309f6f5c-e5ce-49aa-b756-244bb5c45beb"
 
 PKGS_TO_UPLOAD=$(rapids-find-anaconda-uploads.py $RAPIDS_CONDA_BLD_OUTPUT_DIR)
 
-echo $PKGS_TO_UPLOAD
-
 rapids-retry anaconda \
     -t $CONDA_TOKEN \
     upload \
+    --skip-existing \
     --no-progress \
     ${PKGS_TO_UPLOAD}
