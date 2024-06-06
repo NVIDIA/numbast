819220cff065bfabd0692a51b73c3618404840fe
Michael Wang <isVoid@users.noreply.github.com>
2024-06-06 08:54:56 +0800

    Update numba_extensions/numba_extensions/bf16/__init__.py

--- numba_extensions/numba_extensions/bf16/__init__.py
+++ numba_extensions/numba_extensions/bf16/__init__.py
@@ -4,5 +4,4 @@
 from numba_extensions.bf16.bf16_bindings import *  # noqa: F403
 from numba_extensions.bf16.torch_interop import patch_numba
 
-# ProxyTorch = torch_bfloat16_tensor_wrapper()
 patch_numba()
