b46ae07449c1ad5fe5a2fa8266d8ebbf3529b874
Michael Yh Wang <wangm@nvidia.com>
2024-06-13 19:28:31 +0800

    fix non-conda build

---
+++ ast_canopy/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -x -e
+set -o pipefail
+
+echo "Beginning astcanopy Build"
+
+env
+echo ""
+
+apt-get install ninja-build
+
+# Relative to ast_canopy/ <-- This is essential for conda build
+echo "Making directory..."
+mkdir -p ast_canopy/cpp/build
+echo "Entering cpp build..."
+pushd ast_canopy/cpp/build
+echo "Starting cmake config..."
+cmake ${CMAKE_ARGS} \
+    -GNinja \
+    -DCMAKE_BUILD_TYPE:STRING="Release" \
+    -DCMAKE_PREFIX_PATH:PATH="${CONDA_PREFIX}" \
+    -DCMAKE_INSTALL_PREFIX:PATH="${CONDA_PREFIX}" \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DBUILD_STATIC_LIBS:BOOL=OFF \
+    -DCMAKE_CXX_STANDARD:STRING=17 \
+    ../
+echo "cmake build..."
+cmake --build . -j
+echo "cmake install..."
+cmake --install .
+echo "done!"
+popd
+
+echo "pip installing..."
+python -m pip install ast_canopy/ -vv
--- ci/setup_build_test.sh
+++ ci/setup_build_test.sh
@@ -33,12 +33,7 @@ nvcc --version
 rapids-logger "Install Numbast and all submodules"
 
 # Install AST_Canopy, Numbast and extensions
-# FIXME: Current build system cannot auto install libastcanopy.so
-# into system's lib path. While this file is packaged in the wheel,
-# it needs to be moved manually. To be fixed by a new build system.
-pushd ast_canopy/
-build.sh
-popd
+ast_canopy/build.sh
 pip install numbast/
 # bf16 is now in numba_extensions.bf16
 pip install numba_extensions/
