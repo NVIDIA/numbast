a6e99d17aa410dc9e67d250c9a3a11dda4f831a8
Michael Yh Wang <wangm@nvidia.com>
2024-06-13 20:48:40 +0800

    also update setup torch test

--- ci/setup_torch_test.sh
+++ ci/setup_torch_test.sh
@@ -28,12 +28,7 @@ python -m pip install ml-dtypes
 ln -s /usr/bin/clang++-18 /usr/bin/clang++
 
 # Install AST_Canopy, Numbast and extensions
-# FIXME: Current build system cannot auto install libastcanopy.so
-# into system's lib path. While this file is packaged in the wheel,
-# it needs to be moved manually. To be fixed by a new build system.
-pushd ast_canopy/
-build.sh
-popd
+ast_canopy/build.sh
 pip install numbast/
 
 # bf16 is now in numba_extensions.bf16
