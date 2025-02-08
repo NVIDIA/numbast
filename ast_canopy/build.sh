ece6b31297e8801987cfeda643f98be62aaeb181
Michael Wang <isVoid@users.noreply.github.com>
2025-02-08 09:58:30 -0800

    Improve canopy's build system (#89)
    This PR imporves UX of `ast_canopy` build experience.

--- ast_canopy/build.sh
+++ ast_canopy/build.sh
@@ -1,24 +1,68 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -x -e
 set -o pipefail
 
+BUILD_TYPE="Release"
+Editable_Mode="false"
+
+usage() {
+    echo "Usage: ./build.sh [options]"
+    echo ""
+    echo "Options:"
+    echo "  --develop  Install ast_canopy in editable mode"
+    echo "  --debug    Build libastcanopy in debug mode"
+    echo "  --help     Show this help message"
+    echo ""
+    echo "Example usage:"
+    echo "  ./build.sh                   # Build and install in release mode"
+    echo "  ./build.sh --develop         # Build and install in editable mode"
+    echo "  ./build.sh --debug           # Build and install in debug mode"
+    echo "  ./build.sh --develop --debug # Build and install in editable and debug mode"
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --develop)
+            Editable_Mode="true"
+            shift
+            ;;
+        --debug)
+            BUILD_TYPE="Debug"
+            shift
+            ;;
+        --help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            exit 1
+            ;;
+    esac
+done
+
 echo "Beginning astcanopy Build"
+
+# Clean the build cache
+echo "Cleaning ast_canopy/cpp/build cache..."
+rm -rf ast_canopy/cpp/build
+mkdir -p ast_canopy/cpp/build
+echo "Cache cleaned. Starting fresh build..."
 
 env
 echo ""
 
 # Relative to ast_canopy/ <-- This is essential for conda build
-echo "Making directory..."
-mkdir -p ast_canopy/cpp/build
 echo "Entering cpp build..."
 pushd ast_canopy/cpp/build
 echo "Starting cmake config..."
 cmake ${CMAKE_ARGS} \
     -GNinja \
-    -DCMAKE_BUILD_TYPE:STRING="Release" \
+    -DCMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}" \
     -DCMAKE_PREFIX_PATH:PATH="${CONDA_PREFIX}" \
     -DCMAKE_INSTALL_PREFIX:PATH="${CONDA_PREFIX}" \
     -DBUILD_SHARED_LIBS:BOOL=ON \
@@ -32,5 +76,12 @@ cmake --install .
 echo "done!"
 popd
 
-echo "pip installing..."
-python -m pip install ast_canopy/ -vv
+if [ "$Editable_Mode" = "true" ]; then
+    # If it's set, perform an editable install of ast_canopy
+    echo "pip installing in editable mode..."
+    pip install -e ast_canopy/ -vv
+else
+    # If not, perform a normal install
+    echo "pip installing..."
+    python -m pip install ast_canopy/ -vv
+fi
