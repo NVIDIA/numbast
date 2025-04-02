f1174076109d72580ea6d5b7851e58e408d4b806
isVoid <isVoid@users.noreply.github.com>
2025-04-02 14:58:17 -0700

    single source of truth for ast_canopy, and allow importing version from package

--- ast_canopy/ast_canopy/__init__.py
+++ ast_canopy/ast_canopy/__init__.py
@@ -4,3 +4,10 @@
 from ast_canopy.api import (
     parse_declarations_from_source,
 )
+
+from ast_canopy._version import __version__
+
+__all__ = [
+    "__version__",
+    "parse_declarations_from_source",
+]
---
+++ ast_canopy/tests/test_version.py
@@ -0,0 +1,5 @@
+import ast_canopy
+
+
+def test_import():
+    ast_canopy.__version__
