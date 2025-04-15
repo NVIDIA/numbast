6eb1c5fb2a8d8b31ec9548001ca60159a0083f71
isVoid <isVoid@users.noreply.github.com>
2025-04-15 11:11:48 -0700

    using common fixture

---
+++ numbast/src/numbast/tools/tests/conftest.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import shutil
+
+import pytest
+
+from jinja2 import Environment, FileSystemLoader
+
+from click.testing import CliRunner
+
+from numbast.tools.static_binding_generator import static_binding_generator
+
+
+@pytest.fixture
+def run_in_isolated_folder(tmpdir):
+    # Helper to simulate a production environment where configurations are used
+    # Tmp Folder structure:
+    # - /
+    # - config/
+    #   - <config_name>.yml
+    # - output/
+    #   - <header_name>.py
+    #
+    # Test folder structure:
+    # - .
+    # - config
+    #   - <template_a>.yml.j2
+    #   - <template_b>.yml.j2
+    # - <header_a>.cuh
+    # - <header_b>.cuh
+    # - test_a.py
+    # - test_b.py
+    def _run(cfg_template, header, params, ruff_format=False):
+        root = tmpdir
+        config_folder = root.mkdir("config")
+        output_folder = root.mkdir("output")
+        here = os.path.dirname(os.path.abspath(__file__))
+
+        src_data = os.path.join(here, header)
+        target_data = os.path.join(output_folder, header)
+        config_name = cfg_template.replace(".j2", "")
+        config_path = os.path.join(config_folder, config_name)
+        shutil.copy(src_data, target_data)
+
+        params["data"] = target_data
+
+        env = Environment(loader=FileSystemLoader(here))
+        template = env.get_template(os.path.join("config/", cfg_template))
+        config = template.render(params)
+
+        with open(config_path, "w") as f:
+            f.write(config)
+
+        runner = CliRunner()
+        result = runner.invoke(
+            static_binding_generator,
+            [
+                "--cfg-path",
+                config_path,
+                "--output-dir",
+                output_folder,
+                "-fmt",
+                ruff_format,
+            ],
+        )
+
+        binding_name = header.split(".")[0] + ".py"
+        return {
+            "result": result,
+            "output_folder": output_folder,
+            "binding_path": os.path.join(output_folder, binding_name),
+        }
+
+    return _run
--- numbast/src/numbast/tools/tests/test_repro_info.py
+++ numbast/src/numbast/tools/tests/test_repro_info.py
@@ -1,49 +1,21 @@
-import os
-
-from jinja2 import Environment, FileSystemLoader
-
-from click.testing import CliRunner
-from numbast.tools.static_binding_generator import static_binding_generator
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 
-def make_cfg(template, data_cuh, config_folder):
-    env = Environment(loader=FileSystemLoader(os.path.dirname(template)))
-    template = env.get_template(os.path.basename(template))
-    cfg_path = os.path.join(config_folder, "cfg.yml")
-    with open(cfg_path, "w") as f:
-        f.write(template.render({"data": data_cuh}))
-
-    return cfg_path
-
-
-def test_repro_info(tmpdir):
+def test_repro_info(run_in_isolated_folder):
     """Consider both the config and output folder are traversible in the same
     tree, this PR makes sure that reproducible info accurately reflect where
     the config file can be located as a relative path to the binding file.
     """
-    root = tmpdir
-    config_folder = root.mkdir("config")
-    output_folder = root.mkdir("output")
-    here = os.path.dirname(os.path.abspath(__file__))
 
-    data = os.path.join(here, "data.cuh")
-    cfg_template = os.path.join(here, "config/", "cfg.yml.j2")
-    cfg_path = make_cfg(cfg_template, data, config_folder)
+    res = run_in_isolated_folder("cfg.yml.j2", "data.cuh", {}, ruff_format=True)
 
-    runner = CliRunner()
-    result = runner.invoke(
-        static_binding_generator,
-        [
-            "--cfg-path",
-            cfg_path,
-            "--output-dir",
-            output_folder,
-        ],
-    )
+    result = res["result"]
+    binding_path = res["binding_path"]
 
     assert result.exit_code == 0
 
-    with open(os.path.join(output_folder, "data.py")) as f:
+    with open(binding_path) as f:
         bindings = f.readlines()
 
     expected_info = {
