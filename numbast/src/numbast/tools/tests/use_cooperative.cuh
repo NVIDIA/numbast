7c1c14c88e00ab0d51f32f6873a0f1129cac66f8
Michael Wang <13521008+isVoid@users.noreply.github.com>
2025-05-27 19:59:04 +0800

    Add config for functions that requires cooperative launch (#133)
    This PR adds config: `Cooperative Launch Required APIs`, a list of
    strings that specify the names of the function that requires cooperative
    launch.
    
    ---------
    
    Co-authored-by: isVoid <isVoid@users.noreply.github.com>

---
+++ numbast/src/numbast/tools/tests/use_cooperative.cuh
@@ -0,0 +1,27 @@
+// clang-format off
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// clang-format on
+
+#include <cooperative_groups.h>
+#include <cuda/barrier>
+
+namespace cg = cooperative_groups;
+
+__device__ __inline__ void
+_wait_on_tile(cuda::barrier<cuda::thread_scope_block> &tile) {
+  auto token = tile.arrive();
+  tile.wait(std::move(token));
+}
+
+extern "C" __device__ __inline__ int cta_barrier() {
+  auto cta = cg::this_thread_block();
+  cg::thread_block_tile<32> tile = cg::tiled_partition<32>(cta);
+  __shared__ cuda::barrier<cuda::thread_scope_block> barrier;
+  if (threadIdx.x == 0) {
+    init(&barrier, blockDim.x);
+  }
+
+  _wait_on_tile(barrier);
+  return 0;
+}
