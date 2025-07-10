611742e4803073706f775de732775d56b6a363ea
Michael Wang <isVoid@users.noreply.github.com>
2025-07-10 11:04:56 -0700

    fix benchmarks, add missing tests from CI

--- numbast/benchmarks/test_arithmetic.py
+++ numbast/benchmarks/test_arithmetic.py
@@ -2,19 +2,19 @@ import numba.cuda as cuda
 import numpy as np
 from numba import float32
 
-from fp16 import (
-    half,
-    get_shims,
-)
+from numbast_extensions.bf16 import nv_bfloat16, get_shims
+
+import pytest
 
 
+@pytest.mark.skip(reason="benchmark is not run by default")
 def test_arithmetic(benchmark):
     def bench():
         @cuda.jit(link=get_shims())
         def kernel(arith):
             # Binary Arithmetic Operators
-            a = half(1.0)
-            b = half(2.0)
+            a = nv_bfloat16(1.0)
+            b = nv_bfloat16(2.0)
 
             arith[0] = float32(a + b)
             arith[1] = float32(a - b)
