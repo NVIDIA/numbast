d596586a2382f92251725cbd2408abf2ed9d2b32
Lakshay Garg <lakshayg@nvidia.com>
2025-07-29 13:12:10 -0700

    Remove output_name from test

--- numbast/src/numbast/tools/tests/test_skip_prefix.py
+++ numbast/src/numbast/tools/tests/test_skip_prefix.py
@@ -6,12 +6,10 @@ def test_skip_prefix_set(run_in_isolated_folder, arch_str):
     """Tests:
     1. Output binding can be skipped via `Skip Prefix` entry.
     """
-    output_name = "test1.py"
     res = run_in_isolated_folder(
         "skip_prefix.yml.j2",
         "data.cuh",
         {"arch_str": arch_str, "skip_prefix": "m"},
-        output_name=output_name,
         ruff_format=False,
         load_symbols=True,
     )
@@ -25,12 +23,10 @@ def test_skip_prefix_unset(run_in_isolated_folder, arch_str):
     """Tests:
     1. Output binding is unaffected if `Skip Prefix` is not set
     """
-    output_name = "test2.py"
     res = run_in_isolated_folder(
         "skip_prefix.yml.j2",
         "data.cuh",
         {"arch_str": arch_str},
-        output_name=output_name,
         ruff_format=False,
         load_symbols=True,
     )
