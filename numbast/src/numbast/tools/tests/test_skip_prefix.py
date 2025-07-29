64814844d9cb2ca855412607e0c64da98da36907
Lakshay Garg <lakshayg@nvidia.com>
2025-07-29 15:21:13 -0700

    Use separate data file for skip_prefix test

--- numbast/src/numbast/tools/tests/test_skip_prefix.py
+++ numbast/src/numbast/tools/tests/test_skip_prefix.py
@@ -8,15 +8,15 @@ def test_skip_prefix_set(run_in_isolated_folder, arch_str):
     """
     res = run_in_isolated_folder(
         "skip_prefix.yml.j2",
-        "use_cooperative.cuh",
-        {"arch_str": arch_str, "skip_prefix": "cta_"},
+        "data2.cuh",
+        {"arch_str": arch_str, "skip_prefix": "m"},
         ruff_format=False,
         load_symbols=True,
     )
 
     assert res["result"].exit_code == 0
-    assert "cta_barrier" not in res["symbols"]
-    assert "global_barrier_sync" in res["symbols"]
+    assert "add" in res["symbols"]
+    assert "mul" not in res["symbols"]
 
 
 def test_skip_prefix_unset(run_in_isolated_folder, arch_str):
@@ -25,12 +25,12 @@ def test_skip_prefix_unset(run_in_isolated_folder, arch_str):
     """
     res = run_in_isolated_folder(
         "skip_prefix.yml.j2",
-        "use_cooperative.cuh",
+        "data2.cuh",
         {"arch_str": arch_str},
         ruff_format=False,
         load_symbols=True,
     )
 
     assert res["result"].exit_code == 0
-    assert "cta_barrier" in res["symbols"]
-    assert "global_barrier_sync" in res["symbols"]
+    assert "add" in res["symbols"]
+    assert "mul" in res["symbols"]
