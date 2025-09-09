d9dfec733139ad3ca68e29c82fd0378dc0515a61
Andy Jost <ajost@nvidia.com>
2025-09-09 11:17:23 -0700

    Fixes attribute tests; adds a test for a new bug  (#181)
    Functions created by macro expansion are not captured.
    
    See Issue #182

---
+++ ast_canopy/tests/data/pp_function.cu
@@ -0,0 +1,9 @@
+// clang-format off
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// clang-format on
+
+#define DECLARE_FUNCTION(Name)                                                 \
+  int Name() { return 0; }
+
+DECLARE_FUNCTION(func)
