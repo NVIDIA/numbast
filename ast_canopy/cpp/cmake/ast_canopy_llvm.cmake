ea30b8e8c5936c2e9c7d6716a956369f9b517bba
Marcus D. Hanwell <mhanwell@nvidia.com>
2025-09-22 14:43:48 -0400

    Simplify the CMake and be forgiving for shared
    This removes the namespace logic as it appears the LLM hallucinated it,
    and takes a more forgiving approach for shared linkage.

--- ast_canopy/cpp/cmake/ast_canopy_llvm.cmake
+++ ast_canopy/cpp/cmake/ast_canopy_llvm.cmake
@@ -10,28 +10,20 @@ endfunction()
 
 function(ast_canopy_target_link_clang library_target)
   # Check that we are getting the linkage requested for Clang and LLVM
-  if(TARGET Clang::clangTooling)
-    message(STATUS "Modern Clang::clangTooling target found")
-    set(_target_prefix "Clang::")
-  else()
-    message(STATUS "Old clangTooling target fallback")
-    set(_clang_targets ${_clang_targets})
-  endif()
-
   set(_clang_targets
-    ${_target_prefix}clangTooling
-    ${_target_prefix}clangASTMatchers
-    ${_target_prefix}clangAST
-    ${_target_prefix}clangBasic
-    ${_target_prefix}clangFrontend
-    ${_target_prefix}clangSerialization
-    ${_target_prefix}clangDriver
-    ${_target_prefix}clangParse
-    ${_target_prefix}clangSema
-    ${_target_prefix}clangAnalysis
-    ${_target_prefix}clangEdit
-    ${_target_prefix}clangRewrite
-    ${_target_prefix}clangLex
+    clangTooling
+    clangASTMatchers
+    clangAST
+    clangBasic
+    clangFrontend
+    clangSerialization
+    clangDriver
+    clangParse
+    clangSema
+    clangAnalysis
+    clangEdit
+    clangRewrite
+    clangLex
   )
 
   target_link_libraries(${library_target} PRIVATE ${_clang_targets})
@@ -42,15 +34,18 @@ function(ast_canopy_target_link_clang library_target)
   endif()
 
   foreach(_clang_target ${_clang_targets})
-    message(STATUS "Linking ${_clang_target}")
     get_target_property(_clang_target_type ${_clang_target} TYPE)
-    message(STATUS "Type: ${_clang_target_type}")
-    if (NOT "${_clang_target_type}" STREQUAL "${LLVM_LINKAGE}_LIBRARY")
+    if(NOT "${_clang_target_type}" STREQUAL "${LLVM_LINKAGE}_LIBRARY")
       get_target_property(_clang_target_location ${_clang_target} LOCATION)
       message(STATUS "Library file path: ${_clang_target_location}")
-      message(FATAL_ERROR "LLVM_LINKAGE is set to ${LLVM_LINKAGE}, but "
-                          "${_clang_target} is of type ${_clang_target_type}. "
-                          "Please install a correctly compiled Clang/LLVM.")
+      if("${LLVM_LINKAGE}" STREQUAL "STATIC")
+        message(FATAL_ERROR "LLVM_LINKAGE is set to ${LLVM_LINKAGE}, but "
+                            "${_clang_target} is of type ${_clang_target_type}. "
+                            "Please install a correctly compiled Clang/LLVM.")
+      else()
+        message(STATUS "Warning:LLVM_LINKAGE is set to ${LLVM_LINKAGE}, but "
+                       "${_clang_target} is of type ${_clang_target_type}.")
+      endif()
     endif()
   endforeach()
 endfunction()
