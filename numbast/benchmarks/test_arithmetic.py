ec96588a76517d03a2203f86e3b1282347fd9281
Michael Wang <isVoid@users.noreply.github.com>
2025-11-03 14:38:14 -0800

    make numbast_extensions benchmarks tests optional

--- numbast/benchmarks/test_arithmetic.py
+++ numbast/benchmarks/test_arithmetic.py
@@ -2,9 +2,15 @@ import numba.cuda as cuda
 import numpy as np
 from numba import float32
 
-from numbast_extensions.bf16 import nv_bfloat16, get_shims
+import importlib
 
 import pytest
+
+numbast_extensions_spec = importlib.util.find_spec("numbast_extensions")
+if numbast_extensions_spec is None:
+    pytest.skip("numbast_extensions are not installed.")
+
+from numbast_extensions.bf16 import nv_bfloat16, get_shims  # noqa: E402
 
 
 @pytest.mark.skip(reason="benchmark is not run by default")
