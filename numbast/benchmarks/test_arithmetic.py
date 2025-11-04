bb9c7f7e192144237c88bb7347b468049193ab2d
Michael Wang <isVoid@users.noreply.github.com>
2025-11-03 21:20:22 -0800

    optionally import numbast_extensions inside test function

--- numbast/benchmarks/test_arithmetic.py
+++ numbast/benchmarks/test_arithmetic.py
@@ -2,19 +2,13 @@ import numba.cuda as cuda
 import numpy as np
 from numba import float32
 
-import importlib
-
 import pytest
-
-numbast_extensions_spec = importlib.util.find_spec("numbast_extensions")
-if numbast_extensions_spec is None:
-    pytest.skip("numbast_extensions are not installed.")
-
-from numbast_extensions.bf16 import nv_bfloat16, get_shims  # noqa: E402
 
 
 @pytest.mark.skip(reason="benchmark is not run by default")
 def test_arithmetic(benchmark):
+    from numbast_extensions.bf16 import nv_bfloat16, get_shims  # noqa: E402
+
     def bench():
         @cuda.jit(link=get_shims())
         def kernel(arith):
