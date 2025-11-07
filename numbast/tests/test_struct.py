85a244dba37212e6343907cca93a3ca39226898b
Michael Wang <isVoid@users.noreply.github.com>
2025-11-07 10:09:54 -0800

    factor out struct generations

--- numbast/tests/test_struct.py
+++ numbast/tests/test_struct.py
@@ -9,21 +9,43 @@ from numba.core.datamodel import StructModel
 from llvmlite import ir
 
 from ast_canopy import parse_declarations_from_source
-from numbast import bind_cxx_struct, MemoryShimWriter
+from numbast import bind_cxx_structs, MemoryShimWriter
 
 from numba.cuda.descriptor import cuda_target
 
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
+import pytest
 
 
-def test_struct_binding_has_correct_LLVM_type():
-    # This test checks if the bindings of type Foo correctly lowered into
-    # LLVM type { i32, i32, i32 }.
+@pytest.fixture
+def _sample_structs():
+    DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
     p = os.path.join(DATA_FOLDER, "sample_struct.cuh")
     decls = parse_declarations_from_source(p, [p], "sm_80")
     structs = decls.structs
     shim_writer = MemoryShimWriter(f"#include {p}")
-    s = bind_cxx_struct(shim_writer, structs[0], types.Type, StructModel)
+
+    parent_types = {"Foo": types.Type}
+    datamodels = {"Foo": StructModel}
+
+    return bind_cxx_structs(
+        shim_writer, structs, parent_types, datamodels
+    ), shim_writer
+
+
+@pytest.fixture
+def sample_structs(_sample_structs):
+    return _sample_structs[0]
+
+
+@pytest.fixture
+def shim_writer(_sample_structs):
+    return _sample_structs[1]
+
+
+def test_struct_binding_has_correct_LLVM_type(sample_structs):
+    # This test checks if the bindings of type Foo correctly lowered into
+    # LLVM type { i32, i32, i32 }.
+    s = sample_structs[0]
 
     nbty = s._nbtype
     # Get the LLVM type of this front-end type
