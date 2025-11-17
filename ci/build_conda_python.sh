54918465b53f444d3bc984cf773c1f07fa46a8bb
Marcus D. Hanwell <mhanwell@nvidia.com>
2025-11-17 14:44:19 -0500

    Use git tags to generate package versions (#232)
    Closes #221.

--- ci/build_conda_python.sh
+++ ci/build_conda_python.sh
@@ -10,11 +10,16 @@ source rapids-date-string
 
 export CMAKE_GENERATOR=Ninja
 
-# TODO: Once tagging is in place we can use normal git describe.
-# This counts the number of commits since the first commit.
-export PROJECT_VERSION=$(<VERSION)
-export GIT_DESCRIBE_NUMBER=$(git rev-list 5f486a60..HEAD --count)
+# Generate package version from git tags.
+GIT_DESCRIBE_TAG=$(git describe --abbrev=0)
+export PROJECT_VERSION=${GIT_DESCRIBE_TAG:1}
+export GIT_DESCRIBE_NUMBER=$(git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count)
 export GIT_DESCRIBE_HASH=$(git rev-parse --short HEAD)
+
+# If there were commits since the tag this is a dev build.
+if [[ "${GIT_DESCRIBE_NUMBER}" =~ ^[0-9]+$ ]] && [[ "${GIT_DESCRIBE_NUMBER}" -gt 0 ]]; then
+  export PROJECT_VERSION="${PROJECT_VERSION}.dev${GIT_DESCRIBE_NUMBER}"
+fi
 
 rapids-print-env
 
