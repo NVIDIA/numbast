30f84ace50eee03bee469e32ae506a93599fa1c1
Marcus D. Hanwell <mhanwell@nvidia.com>
2025-11-17 12:45:15 -0500

    Augment conda versions for dev builds

--- ci/build_conda_python.sh
+++ ci/build_conda_python.sh
@@ -16,6 +16,11 @@ export PROJECT_VERSION=${GIT_DESCRIBE_TAG:1}
 export GIT_DESCRIBE_NUMBER=$(git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count)
 export GIT_DESCRIBE_HASH=$(git rev-parse --short HEAD)
 
+# If there were commits since the tag this is a dev build.
+if [[ "${GIT_DESCRIBE_NUMBER}" =~ ^[0-9]+$ ]] && [[ "${GIT_DESCRIBE_NUMBER}" -gt 0 ]]; then
+  export PROJECT_VERSION="${PROJECT_VERSION}.dev${GIT_DESCRIBE_NUMBER}"
+fi
+
 rapids-print-env
 
 rapids-logger "Begin py build"
