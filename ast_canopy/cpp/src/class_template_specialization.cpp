7e9f400922a0785a536d391c3e1af10e72836a54
Michael Wang <isVoid@users.noreply.github.com>
2025-11-17 16:40:08 -0800

    update per file license

--- ast_canopy/cpp/src/class_template_specialization.cpp
+++ ast_canopy/cpp/src/class_template_specialization.cpp
@@ -1,3 +1,8 @@
+// clang-format off
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// clang-format on
+
 #include <ast_canopy/ast_canopy.hpp>
 
 #include <clang/AST/DeclTemplate.h>
--- numbast/tests/data/sample_class_template.cuh
+++ numbast/tests/data/sample_class_template.cuh
@@ -1,3 +1,8 @@
+// clang-format off
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// clang-format on
+
 #pragma once
 
 template <typename T, int BLOCK_DIM_X> struct BlockScan {
