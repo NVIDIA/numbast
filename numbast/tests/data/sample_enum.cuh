61e15d303752a7a25e5c6552b1ecd00eae549c29
Michael Wang <13521008+isVoid@users.noreply.github.com>
2025-11-24 12:57:38 -0800

    Allow Function Bindings To Accept Enum as Argument (#250)
    When enums bindings are generated from C++, today's function argument
    fails to accept it as an input. Example:
    
    ```c++
    enum Fruit {
        Apple,
        Banana,
    };
    
    void __device__ eat(Fruit fruit) {}
    ```
    
    This PR enables the following usage:
    
    ```python
    from bindings import eat, Fruit
    
    @cuda.jit
    def kernel():
        eat(Fruit.Apple)
    ```
    
    ---------
    
    Co-authored-by: Michael Wang <isVoid@users.noreply.github.com>

--- numbast/tests/data/sample_enum.cuh
+++ numbast/tests/data/sample_enum.cuh
@@ -5,20 +5,16 @@
 
 enum Fruit { Apple = 1, Banana = 3, Orange = 5 };
 
-enum class Animal { Cat, Dog, Horse };
-
-void __device__ feed(Animal animal, int *out) {
-  switch (animal) {
-  case Animal::Cat:
+void __device__ eat(Fruit fruit, int *out) {
+  switch (fruit) {
+  case Fruit::Apple:
     out[0] = 1;
     break;
-  case Animal::Dog:
+  case Fruit::Banana:
     out[0] = 2;
     break;
-  case Animal::Horse:
+  case Fruit::Orange:
     out[0] = 3;
-    break;
-  default:
     break;
   }
 }
