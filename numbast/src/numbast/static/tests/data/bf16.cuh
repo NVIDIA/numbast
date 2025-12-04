0a3e183cbca2ccd4cf2c084b438046c6d82fea70
Michael Wang <isVoid@users.noreply.github.com>
2025-12-03 19:44:01 -0800

    add bf16_raw type support in Numbast

--- numbast/src/numbast/static/tests/data/bf16.cuh
+++ numbast/src/numbast/static/tests/data/bf16.cuh
@@ -10,4 +10,12 @@ nv_bfloat16 inline __device__ add(nv_bfloat16 a, nv_bfloat16 b) {
   return a + b;
 }
 
+__nv_bfloat16_raw inline __device__ bf16_to_raw(nv_bfloat16 a) {
+  return __nv_bfloat16_raw(a);
+}
+
+nv_bfloat16 inline __device__ bf16_from_raw(__nv_bfloat16_raw a) {
+  return __nv_bfloat16(a);
+}
+
 #endif
