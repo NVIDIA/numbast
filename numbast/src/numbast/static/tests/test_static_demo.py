56f62d600f5e2b2bd36acd4e2a40588c4df1ff7f
Michael Wang <isVoid@users.noreply.github.com>
2025-12-04 16:17:36 -0800

    fix: bug in static demo test

--- numbast/src/numbast/static/tests/test_static_demo.py
+++ numbast/src/numbast/static/tests/test_static_demo.py
@@ -3,6 +3,8 @@
 
 import pytest
 
+import numpy as np
+
 from numba import cuda
 from numba.cuda.types import Number, float64
 from numba.cuda.datamodel import PrimitiveModel
@@ -53,4 +55,5 @@ def test_demo(decl):
 
     arr = device_array((1,), "float64")
     kernel[1, 1](arr)
-    assert all(arr.copy_to_host() == [])
+    host = arr.copy_to_host()
+    assert np.allclose(host, [3.14], rtol=1e-3, atol=1e-3)
