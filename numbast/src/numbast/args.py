aefb4a6528395cf3aabb509434a046bb590b3363
Michael Wang <isVoid@users.noreply.github.com>
2025-12-22 22:59:43 -0800

    remove prepare_args completely

--- numbast/src/numbast/args.py
+++ numbast/src/numbast/args.py
@@ -1,54 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import numba.cuda.types as nbtypes
 from numba.cuda.target import CUDATargetContext
 
 from llvmlite import ir
-
-
-def prepare_args(context, builder, sig, args, ignore_first=False):
-    """
-    Prepare arguments to be passed across FFI calling convention.
-
-    Currently the calling convention is:
-    All arguments are copied onto the stack and passed as pointers across FFI.
-
-    Parameters
-    ----------
-    context : numba.cuda.context.CUDATargetContext
-        The target context to prepare arguments for.
-    builder : llvmlite.IRBuilder
-        The llvmlite IR builder to use.
-    sig : numba.cuda.typing.templates.Signature
-        The signature of the function to prepare arguments for.
-    args : list
-        The actual arguments to prepare.
-    ignore_first : bool
-        Whether to ignore the first argument. Used by class template lowering when
-        the first argument is a numba typeof reference that's unused by actual
-        lowering.
-
-    Returns
-    -------
-    ptrs : list
-        The pointers to the arguments.
-    """
-
-    argtys = sig.args[1:] if ignore_first else sig.args
-    args = args[1:] if ignore_first else args
-
-    processed_argtys: list[nbtypes.Type] = list(argtys)
-    processed_args = list(args)
-
-    ptrs = [
-        builder.alloca(context.get_value_type(argty))
-        for argty in processed_argtys
-    ]
-    for ptr, argty, arg in zip(ptrs, processed_argtys, processed_args):
-        builder.store(arg, ptr, align=getattr(argty, "alignof_", None))
-
-    return ptrs
 
 
 def prepare_ir_types(
